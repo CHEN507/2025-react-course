620d5acda37d7cd3177778fef0cf2077e128ec53
chen <75985885+CHEN507@users.noreply.github.com>
2026-02-05 00:15:21 +0800

    chore: configure vite base path and resolve alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/2025-react-course/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      assets: fileURLToPath(new URL('./src/assets', import.meta.url)),
+    },
+  },
+  server: {
+    open: true,
+  },
 });
